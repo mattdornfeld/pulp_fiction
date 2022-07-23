6fad71480bb80270bc17fc0dc3f0cb71542f6ff0
Matthew Dornfeld <mdornfe1@gmail.com>
2022-07-23 18:11:27 -0400

    Add functionality to test, plan, and apply Terraform changes using Bazel and CircleCI (#2)
    * Add terraform state to gitignore
    
    * Add terraform_rules to WORKSPACE
    
    * Add toy Terraform deploy
    
    * Add functionality to test and plan terraform changes as part of ci

---
+++ terraform/main.tf
@@ -0,0 +1,9 @@
+# local provider
+resource "local_file" "hello" {
+  content  = "Hello, world!"
+  filename = "/tmp/rules_terraform/hello.txt"
+}
+
+#module "time" {
+#  source = "../time_module"
+#}
