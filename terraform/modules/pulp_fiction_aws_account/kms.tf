32e91aec73b614534af4cc299d3c21b189c981a0
Matthew Dornfeld <mdornfe1@gmail.com>
2022-08-01 21:37:43 -0400

    Create pulp_fiction_backend_service database with credentials (#11)
    * Moved secrets subproject to toplevel so it can be used by multiple subprojects
    
    * Create backed database with encrypted credentials and create iam role to access kms key for decrypting
    
    * Add encrypted secrets to pulp_fiction_backend_service Docker image

--- terraform/modules/pulp_fiction_aws_account/kms.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_kms_key" "pulp_fiction_secrets_encryption" {
-  description = "pulp-fiction-secrets-encryption"
-}
---
+++ terraform/modules/pulp_fiction_aws_account/pulp_fiction_backend_service_kms.tf
@@ -0,0 +1,3 @@
+resource "aws_kms_key" "pulp_fiction_backend_service_secrets_encryption" {
+  description = "pulp-fiction-backend-service-secrets-encryption"
+}
