a2708b3de691e40d8cf0ee72f609df5ca3aec09b
Matthew Dornfeld <mdornfe1@gmail.com>
2022-09-05 15:12:01 -0400

    Begin writing code to pass in grpc client as a dependency to app

--- pulp_fiction_ios_app/PulpFictionApp/PulpFictionApp.swift
+++ pulp_fiction_ios_app/PulpFictionApp/PulpFictionApp.swift
@@ -6,23 +6,52 @@
 //
 //
 
+import Bow
+import BowEffects
 import ComposableArchitecture
 import SwiftUI
 
+struct ExternalConnections {
+    let localImageStore: LocalImageStore
+    let backendMessenger: BackendMessenger
+
+    static func create() -> Result<ExternalConnections, PulpFictionStartupError> {
+        let pulpFictionClientProtocolIO = IO<PulpFictionStartupError, PulpFictionClientProtocol>.var()
+
+        return binding(
+            pulpFictionClientProtocolIO <- GrpcUtils.buildTestPulpFictionClientProtocol(),
+            yield: ExternalConnections(
+                localImageStore: LocalImageStore()!,
+                backendMessenger: BackendMessenger(pulpFictionClientProtocol: pulpFictionClientProtocolIO.get)
+            )
+        )^
+            .unsafeRunSyncEither()
+            .toResult()
+    }
+}
+
 @main
 struct PulpFictionApp: App {
-    var body: some Scene {
-        // TODO: Add error handling here so error message is displayed to user if connection to database fails
-        let localImageStore = LocalImageStore()!
+    let externalConnectionsResult = ExternalConnections.create()
 
+    var body: some Scene {
         WindowGroup {
+            buildView()
+        }
+    }
+
+    @ViewBuilder private func buildView() -> some View {
+        switch externalConnectionsResult {
+        case let .success(externalConnections):
             NavigationView {
                 VStack {
-                    NavigationLink("create", destination: PostCreatorView(localImageStore: localImageStore))
+                    NavigationLink("create", destination: PostCreatorView(localImageStore: externalConnections.localImageStore))
                     Divider()
-                    NavigationLink("feed", destination: ScrollingContentView(localImageStore: localImageStore))
+                    NavigationLink("feed", destination: ScrollingContentView(localImageStore: externalConnections.localImageStore))
                 }
             }
+        case let .failure(pulpFictionStartupError):
+            NavigationView {}
         }
     }
 }
--- pulp_fiction_ios_app/PulpFictionApp/Utils/PulpFictionUtils.swift
+++ pulp_fiction_ios_app/PulpFictionApp/Utils/PulpFictionUtils.swift
@@ -1,5 +1,8 @@
 //
-// Created by Matthew Dornfeld on 4/29/22.
+//  PulpFictionUtils.swift
+//  _idx_build_source_21C0F9E3_ios_min15.0
+//
+//  Created by Matthew Dornfeld on 9/5/22.
 //
 
 import Foundation
