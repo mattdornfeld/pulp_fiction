15c6c52029c44042c92db5dac1a2bde5de98ea1f
Matthew Dornfeld <mdornfe1@gmail.com>
2022-11-20 17:12:37 -0500

    Encapsulated EmptyNavigationLink into its own view

--- pulp_fiction_ios_app/PulpFictionAppSource/ViewComponents/EditTextView.swift
+++ pulp_fiction_ios_app/PulpFictionAppSource/ViewComponents/EditTextView.swift
@@ -10,6 +10,8 @@ import Foundation
 import SwiftUI
 
 struct EditTextReducer: ReducerProtocol {
+    let maxTextSize: Int
+
     struct State: Equatable {
         var text: String = ""
         var showInvalidInputAlert: Bool = false
@@ -23,7 +25,7 @@ struct EditTextReducer: ReducerProtocol {
     func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
         switch action {
         case let .updateText(newText):
-            state.text = newText
+            state.text = String(newText.prefix(maxTextSize))
             return .none
         case let .updateShowInvalidInputAlert(newShowInvalidInputAlert):
             state.showInvalidInputAlert = newShowInvalidInputAlert
@@ -38,11 +40,27 @@ struct EditTextView: View {
     let keyboardType: UIKeyboardType
     let createButtonAction: (String) -> Void
     let validateTextAction: (String) -> Bool
-    private let store: ComposableArchitecture.StoreOf<EditTextReducer> = Store(
-        initialState: EditTextReducer.State(),
-        reducer: EditTextReducer()
-    )
+    private let store: ComposableArchitecture.StoreOf<EditTextReducer>
     @FocusState private var isInputTextFieldFocused: Bool
+
+    init(
+        prompt: String,
+        createButtonLabel: String,
+        keyboardType: UIKeyboardType,
+        maxTextSize: Int = 10000,
+        createButtonAction: @escaping (String) -> Void,
+        validateTextAction: @escaping (String) -> Bool
+    ) {
+        self.prompt = prompt
+        self.createButtonLabel = createButtonLabel
+        self.keyboardType = keyboardType
+        self.createButtonAction = createButtonAction
+        self.validateTextAction = validateTextAction
+        store = Store(
+            initialState: EditTextReducer.State(),
+            reducer: EditTextReducer(maxTextSize: maxTextSize)
+        )
+    }
 
     var body: some View {
         WithViewStore(store) { viewStore in
--- pulp_fiction_ios_app/PulpFictionAppSource/Views/PostCreator/CommentCreatorView.swift
+++ pulp_fiction_ios_app/PulpFictionAppSource/Views/PostCreator/CommentCreatorView.swift
@@ -5,76 +5,25 @@
 //  Created by Matthew Dornfeld on 10/28/22.
 //
 
-import ComposableArchitecture
 import Foundation
 import SwiftUI
 
-/// Reducer for CommentCreatorView
-struct CommentCreatorReducer: ReducerProtocol {
-    private let maxCommentSize: Int = 100
-
-    struct State: Equatable {
-        /// Comment being created
-        var comment: String = ""
-    }
-
-    enum Action {
-        /// Updates the comment as new characters are typed
-        case updateComment(String)
-        /// Posts the comment
-        case postComment(() -> Void)
-    }
-
-    func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
-        switch action {
-        case let .updateComment(newComment):
-            state.comment = String(newComment.prefix(maxCommentSize))
-            return .none
-
-        case let .postComment(backAction):
-            if state.comment.count == 0 {
-                return .none
-            }
-
-            print(state.comment)
-            backAction()
-            return .none
-        }
-    }
-}
-
 /// View for creating and posting comments
 struct CommentCreatorView: View {
-    private let store: ComposableArchitecture.StoreOf<CommentCreatorReducer> = Store(
-        initialState: CommentCreatorReducer.State(),
-        reducer: CommentCreatorReducer()
-    )
-    @FocusState private var isInputCommentFieldFocused: Bool
     @Environment(\.presentationMode) private var presentationMode: Binding<PresentationMode>
 
     var body: some View {
-        WithViewStore(store) { viewStore in
-            VStack {
-                TextField(
-                    "Write a comment",
-                    text: viewStore.binding(
-                        get: \.comment,
-                        send: { newComment in .updateComment(newComment) }
-                    ),
-                    prompt: Text("Write a comment")
-                )
-                .foregroundColor(.gray)
-                .focused($isInputCommentFieldFocused)
-                Spacer()
-            }
-            .onAppear {
-                isInputCommentFieldFocused = true
-            }
-            .toolbar {
-                TextCreatorTopNavigationBar(createButtonLabel: "Comment") {
-                    viewStore.send(.postComment { self.presentationMode.wrappedValue.dismiss() })
+        EditTextView(
+            prompt: "Write a comment",
+            createButtonLabel: "Comment",
+            createButtonAction: { comment in
+                if comment.count == 0 {
+                    return
                 }
+
+                print(comment)
+                self.presentationMode.wrappedValue.dismiss()
             }
-        }
+        )
     }
 }
