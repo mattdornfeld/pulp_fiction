e8d0ba8969785fe40978ba07ab576c14d33bf473
Matthew Dornfeld <mdornfe1@gmail.com>
2022-12-24 01:38:43 -0500

    Change name of Login endpoint to CreateLoginSession

--- pulp_fiction_ios_app/PulpFictionAppSource/Models/ProtoAliases.swift
+++ pulp_fiction_ios_app/PulpFictionAppSource/Models/ProtoAliases.swift
@@ -11,12 +11,12 @@ import protos_pulp_fiction_grpc_swift
 import protos_pulp_fiction_proto
 import SwiftProtobuf
 
+public typealias CreateLoginSessionResponse = PulpFiction_Protos_CreateLoginSessionResponse
 public typealias CreatePostRequest = PulpFiction_Protos_CreatePostRequest
 public typealias CreatePostResponse = PulpFiction_Protos_CreatePostResponse
 public typealias GetFeedRequest = PulpFiction_Protos_GetFeedRequest
 public typealias GetFeedResponse = PulpFiction_Protos_GetFeedResponse
 public typealias ImagePost = PulpFiction_Protos_Post.ImagePost
-public typealias LoginResponse = PulpFiction_Protos_LoginResponse
 public typealias Post = PulpFiction_Protos_Post
 public typealias PulpFictionClientProtocol = PulpFiction_Protos_PulpFictionClientProtocol
 public typealias PulpFictionServiceClient = PulpFiction_Protos_PulpFictionClient
